8bacffe70a33a3641f4de10db85adc9e73841126
Jose Pedro Magalhaes <jpm@cs.ox.ac.uk>
2011-05-25 07:18:59 +0000

    Now fully working with both ghc-7.0 and ghc-7.1.

--- examples/compareOutputs.bat
+++
@@ -1,7 +0,0 @@
-:: Windows only, but it should be clear what's being done here.
-ehc -i../src examples && ghc --odir=../dist -i../src -o examplesGHC.exe --make examples || GOTO EXIT
-examplesGHC.exe > outGHC
-examples.exe > outEHC
-diff outGHC outEHC -b
-
-:EXIT
