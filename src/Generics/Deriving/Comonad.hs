c3611dea98879d47e581d3772ac1a8627b75f1ff
Christian Conkle <christian@conkle.org>
2015-01-24 14:28:36 -0800

    Implement `Generics.Deriving.Comonad`.
    Several `GComonad'` instance needs access to `GFunctor'` member functions. To expose those functions, I moved Generics.Deriving.Functor to an internal module that exposes the implementation class, and created a thin wrapper that only exports the public API.

---
+++ src/Generics/Deriving/Comonad.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE CPP #-}
+#if __GLASGOW_HASKELL__ >= 701
+{-# LANGUAGE DefaultSignatures #-}
+#endif
+
+module Generics.Deriving.Comonad (
+  -- * GComonad class
+  GComonad(..)
+  
+  -- * Default methods
+  , gduplicatedefault
+  , gextractdefault
+  
+  ) where
+
+import Generics.Deriving.Base
+import Generics.Deriving.Instances ()
+import Generics.Deriving.Internal.Functor
+
+class GComonad' w where
+  gduplicate' :: w a -> w (w a)
+  gextract' :: w a -> a
+  
+instance GComonad' Par1 where
+  gduplicate' = Par1
+  {-# INLINE gduplicate' #-}
+  gextract'   = unPar1
+  {-# INLINE gextract' #-}
+
+instance (GFunctor' a, GFunctor' w, GComonad' w) => GComonad' (a :*: w) where
+  gduplicate' w@(a :*: b) = gmap' (const w) a :*: gmap' (const w) b
+  gextract' (_ :*: b) = gextract' b
+
+instance (GFunctor' f, GFunctor' g, GComonad' f, GComonad' g) => GComonad' (f :+: g) where
+  gduplicate' w@(L1 a) = L1 . gmap' (const w) $ a
+  gduplicate' w@(R1 a) = R1 . gmap' (const w) $ a
+  gextract' (L1 a) = gextract' a
+  gextract' (R1 a) = gextract' a
+  
+instance (GComonad' f, GFunctor' f) => GComonad' (M1 i c f) where
+  gduplicate' w@(M1 a) = M1 (gmap' (const w) a)
+  gextract' (M1 a) = gextract' a
+{- |
+
+Generically derived comonads.
+
+Instances are available for any type of the form 
+
+@
+data W a =
+    C1 [t11 t12 .. t1N] a
+ [| C2 [t21 t22 .. t2N] a
+  | .. ]
+  deriving ('Generic1')
+instance 'GFunctor' W 
+@
+
+In other words, each constructor must include @a@ in the rightmost position.
+
+If @a@ appears in other positions, it will be ignored by 'gextract'.
+
+-}
+class GComonad w where
+  gduplicate :: w a -> w (w a)
+  default gduplicate :: (Generic1 w, GFunctor' (Rep1 w), GComonad' (Rep1 w))
+                     => w a -> w (w a)
+  gduplicate = to1 . gmap' to1 . gduplicate' . from1
+  
+  gextract :: w a -> a
+  default gextract :: (Generic1 w, GComonad' (Rep1 w))
+                   => w a -> a
+  gextract = gextract' . from1
+
+gduplicatedefault :: (Generic1 w, GFunctor' (Rep1 w), GComonad' (Rep1 w))
+                  => w a -> w (w a)
+gduplicatedefault = to1 . gmap' to1 . gduplicate' . from1
+
+gextractdefault :: (Generic1 w, GComonad' (Rep1 w))
+                => w a -> a
+gextractdefault = gextract' . from1
+
--- src/Generics/Deriving/Functor.hs
+++ src/Generics/Deriving/Functor.hs
@@ -1,9 +1,3 @@
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE CPP #-}
-#if __GLASGOW_HASKELL__ >= 701
-{-# LANGUAGE DefaultSignatures #-}
-#endif
 
 module Generics.Deriving.Functor (
   -- * GFunctor class
@@ -14,57 +8,4 @@ module Generics.Deriving.Functor (
 
   ) where
 
-import Generics.Deriving.Base
-import Generics.Deriving.Instances ()
-
---------------------------------------------------------------------------------
--- Generic fmap
---------------------------------------------------------------------------------
-
-class GFunctor' f where
-  gmap' :: (a -> b) -> f a -> f b
-
-instance GFunctor' U1 where
-  gmap' _ U1 = U1
-
-instance GFunctor' Par1 where
-  gmap' f (Par1 a) = Par1 (f a)
-
-instance GFunctor' (K1 i c) where
-  gmap' _ (K1 a) = K1 a
-
-instance (GFunctor f) => GFunctor' (Rec1 f) where
-  gmap' f (Rec1 a) = Rec1 (gmap f a)
-
-instance (GFunctor' f) => GFunctor' (M1 i c f) where
-  gmap' f (M1 a) = M1 (gmap' f a)
-
-instance (GFunctor' f, GFunctor' g) => GFunctor' (f :+: g) where
-  gmap' f (L1 a) = L1 (gmap' f a)
-  gmap' f (R1 a) = R1 (gmap' f a)
-
-instance (GFunctor' f, GFunctor' g) => GFunctor' (f :*: g) where
-  gmap' f (a :*: b) = gmap' f a :*: gmap' f b
-
-instance (GFunctor f, GFunctor' g) => GFunctor' (f :.: g) where
-  gmap' f (Comp1 x) = Comp1 (gmap (gmap' f) x)
-
-
-class GFunctor f where
-  gmap :: (a -> b) -> f a -> f b
-#if __GLASGOW_HASKELL__ >= 701
-  default gmap :: (Generic1 f, GFunctor' (Rep1 f))
-               => (a -> b) -> f a -> f b
-  gmap = gmapdefault
-#endif
-
-gmapdefault :: (Generic1 f, GFunctor' (Rep1 f))
-            => (a -> b) -> f a -> f b
-gmapdefault f = to1 . gmap' f . from1
-
--- Base types instances
-instance GFunctor Maybe where
-  gmap = gmapdefault
-
-instance GFunctor [] where
-  gmap = gmapdefault
+import Generics.Deriving.Internal.Functor
