eb8f9012a24683bffa10687b3421eff4edb68f57
RyanGlScott <ryan.gl.scott@ku.edu>
2015-04-29 10:28:03 -0500

    Bring up-to-date with base

--- src/Generics/Deriving/Instances.hs
+++ src/Generics/Deriving/Instances.hs
@@ -19,6 +19,7 @@ module Generics.Deriving.Instances (
   ) where
 
 #if __GLASGOW_HASKELL__ < 705
+import Data.Orphans ()
 import Generics.Deriving.Base
 #endif
 
@@ -41,94 +42,150 @@ instance Generic1 (Rec1 f) (Rep1Rec1 f) where
 -}
 -- Kind *
 
-type Rep0Char = Rec0 Char
+---- Taken from GHC.Generics
+
+-- Char
+data D_Char
+data C_Char
+
+instance Datatype D_Char where
+  datatypeName _ = "Char"
+  moduleName   _ = "GHC.Base"
+
+instance Constructor C_Char where
+  conName _ = "" -- JPM: I'm not sure this is the right implementation...
+
+type Rep0Char = D1 D_Char (C1 C_Char (S1 NoSelector (Rec0 Char)))
 instance Generic Char where
   type Rep Char = Rep0Char
-  from = K1
-  to = unK1
+  from x = M1 (M1 (M1 (K1 x)))
+  to (M1 (M1 (M1 (K1 x)))) = x
 
-type Rep0Int = Rec0 Int
+-- Int
+data D_Int
+data C_Int
+
+instance Datatype D_Int where
+  datatypeName _ = "Int"
+  moduleName   _ = "GHC.Int"
+
+instance Constructor C_Int where
+  conName _ = "" -- JPM: I'm not sure this is the right implementation...
+
+type Rep0Int = D1 D_Int (C1 C_Int (S1 NoSelector (Rec0 Int)))
 instance Generic Int where
   type Rep Int = Rep0Int
-  from = K1
-  to = unK1
+  from x = M1 (M1 (M1 (K1 x)))
+  to (M1 (M1 (M1 (K1 x)))) = x
 
-type Rep0Float = Rec0 Float
+
+-- Float
+data D_Float
+data C_Float
+
+instance Datatype D_Float where
+  datatypeName _ = "Float"
+  moduleName   _ = "GHC.Float"
+
+instance Constructor C_Float where
+  conName _ = "" -- JPM: I'm not sure this is the right implementation...
+
+type Rep0Float = D1 D_Float (C1 C_Float (S1 NoSelector (Rec0 Float)))
 instance Generic Float where
   type Rep Float = Rep0Float
-  from = K1
-  to = unK1
+  from x = M1 (M1 (M1 (K1 x)))
+  to (M1 (M1 (M1 (K1 x)))) = x
 
 -- etc...
 
 -- Kind * -> *
 
-type Rep0Maybe a = D1 Maybe_ (C1 Nothing_ U1 :+: C1 Just_ (Par0 a))
+-- Maybe a
+type Rep0Maybe a = D1 D1Maybe (C1 C1_0Maybe U1
+                           :+: C1 C1_1Maybe (S1 NoSelector (Rec0 a)))
 instance Generic (Maybe a) where
-  type Rep (Maybe a) = Rep0Maybe a
-  from Nothing  = M1 (L1 (M1 U1))
-  from (Just x) = M1 (R1 (M1 (K1 x)))
-  to (M1 (L1 (M1 U1)))     = Nothing
-  to (M1 (R1 (M1 (K1 x)))) = Just x
+    type Rep (Maybe a) = Rep0Maybe a
 
-type Rep0List a = D1 List__ ((C1 Nil__ U1) :+: (C1 Cons__ (Par0 a :*: Rec0 [a])))
+    from Nothing  = M1 (L1 (M1 U1))
+    from (Just j) = M1 (R1 (M1 (M1 (K1 j))))
+
+    to (M1 (L1 (M1 U1)))          = Nothing
+    to (M1 (R1 (M1 (M1 (K1 j))))) = Just j
+
+data D1Maybe
+data C1_0Maybe
+data C1_1Maybe
+
+instance Datatype D1Maybe where
+    datatypeName _ = "Maybe"
+    moduleName   _ = "Data.Maybe"
+
+instance Constructor C1_0Maybe where
+    conName _ = "Nothing"
+
+instance Constructor C1_1Maybe where
+    conName _ = "Just"
+
+-- [a]
+type Rep0List a = D1 D1List (C1 C1_0List U1
+                         :+: C1 C1_1List (S1 NoSelector (Rec0 a)
+                                      :*: S1 NoSelector (Rec0 [a])))
 instance Generic [a] where
-  type Rep [a] = Rep0List a
-  from []    = M1 (L1 (M1 U1))
-  from (h:t) = M1 (R1 (M1 (K1 h :*: K1 t)))
-  to (M1 (L1 (M1 U1)))              = []
-  to (M1 (R1 (M1 (K1 h :*: K1 t)))) = h : t
+    type Rep [a] = Rep0List a
+
+    from []    = M1 (L1 (M1 U1))
+    from (h:t) = M1 (R1 (M1 (M1 (K1 h) :*: M1 (K1 t))))
+
+    to (M1 (L1 (M1 U1)))                        = []
+    to (M1 (R1 (M1 (M1 (K1 h) :*: M1 (K1 t))))) = h : t
+
+data D1List
+data C1_0List
+data C1_1List
+
+instance Datatype D1List where
+    datatypeName _ = "[]"
+    moduleName   _ = "GHC.Types"
+
+instance Constructor C1_0List where
+    conName _ = "[]"
+
+instance Constructor C1_1List where
+    conName   _ = ":"
+    conFixity _ = Infix RightAssociative 5
 #endif
 
-#if __GLASGOW_HASKELL__ < 705
+#if __GLASGOW_HASKELL__ >= 701 && __GLASGOW_HASKELL__ < 705
 -- GHC 7.2 and 7.4 still need these instances; 7.6 doesn't
 
-data Maybe_
-data Nothing_
-data Just_
-
-instance Datatype Maybe_ where
-  datatypeName _ = "Maybe"
-  moduleName   _ = "Representation"
-
-instance Constructor Nothing_ where
-  conName _ = "Nothing"
-
-instance Constructor Just_ where
-  conName _ = "Just"
-
-
-type Rep1Maybe = D1 Maybe_ (C1 Nothing_ U1 :+: C1 Just_ Par1)
-instance Generic1 Maybe where
-  type Rep1 Maybe = Rep1Maybe
-  from1 Nothing  = M1 (L1 (M1 U1))
-  from1 (Just x) = M1 (R1 (M1 (Par1 x)))
-  to1 (M1 (L1 (M1 U1)))       = Nothing
-  to1 (M1 (R1 (M1 (Par1 x)))) = Just x
-
-
-data List__
-data Nil__
-data Cons__
-
-instance Datatype List__ where
-  datatypeName _ = "[]"
-  moduleName   _ = "Data.List"
-
-instance Constructor Nil__  where conName _ = "[]"
-instance Constructor Cons__ where
-  conName   _ = ":"
-  conFixity _ = Infix RightAssociative 5
-
-
-type Rep1List = D1 List__ ((C1 Nil__ U1) :+: (C1 Cons__ (Par1 :*: Rec1 [])))
-instance Generic1 [] where
-  type Rep1 [] = Rep1List
-  from1 []    = M1 (L1 (M1 U1))
-  from1 (h:t) = M1 (R1 (M1 (Par1 h :*: Rec1 t)))
-  to1 (M1 (L1 (M1 U1)))                  = []
-  to1 (M1 (R1 (M1 (Par1 h :*: Rec1 t)))) = h : t
+type Rep1Maybe = Rep1 Maybe
+type Rep1List  = Rep1 []
 
 -- etc...
+
+#elif __GLASGOW_HASKELL__ < 701
+
+type Rep1Maybe = D1 D1Maybe (C1 C1_0Maybe U1
+                         :+: C1 C1_1Maybe (S1 NoSelector Par1))
+instance Generic1 Maybe where
+    type Rep1 Maybe = Rep1Maybe
+
+    from1 Nothing  = M1 (L1 (M1 U1))
+    from1 (Just j) = M1 (R1 (M1 (M1 (Par1 j))))
+
+    to1 (M1 (L1 (M1 U1)))            = Nothing
+    to1 (M1 (R1 (M1 (M1 (Par1 j))))) = Just j
+
+type Rep1List = D1 D1List (C1 C1_0List U1 :+:
+                           C1 C1_1List (S1 NoSelector Par1
+                                    :*: S1 NoSelector (Rec1 [])))
+instance Generic1 [] where
+    type Rep1 [] = Rep1List
+
+    from1 []    = M1 (L1 (M1 U1))
+    from1 (h:t) = M1 (R1 (M1 (M1 (Par1 h) :*: M1 (Rec1 t))))
+
+    to1 (M1 (L1 (M1 U1)))                            = []
+    to1 (M1 (R1 (M1 (M1 (Par1 h) :*: M1 (Rec1 t))))) = h : t
 
 #endif
