adbd02cf6fab0cc432ce3298dcc4bfae8fa946f5
RyanGlScott <ryan.gl.scott@gmail.com>
2015-11-08 11:57:52 -0800

    More Show instances

--- src/Generics/Deriving/Show.hs
+++ src/Generics/Deriving/Show.hs
@@ -4,8 +4,13 @@
 {-# LANGUAGE MagicHash #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+
 #if __GLASGOW_HASKELL__ >= 701
 {-# LANGUAGE DefaultSignatures #-}
+#endif
+
+#if __GLASGOW_HASKELL__ < 709
+{-# LANGUAGE OverlappingInstances #-}
 #endif
 
 module Generics.Deriving.Show (
@@ -17,14 +22,46 @@ module Generics.Deriving.Show (
 
   ) where
 
+import Control.Applicative (Const, ZipList)
+
+import Data.Char (GeneralCategory)
+import Data.Int
+import Data.Monoid (All, Any, Dual, First, Last, Product, Sum)
+import Data.Word
+
+import Foreign.C.Types
+import Foreign.ForeignPtr (ForeignPtr)
+import Foreign.Ptr
+
 import Generics.Deriving.Base
 import Generics.Deriving.Instances ()
 
-import GHC.Exts
+import GHC.Exts hiding (Any)
+
+import System.Exit (ExitCode)
+import System.IO (BufferMode, Handle, HandlePosn, IOMode, SeekMode)
+import System.IO.Error (IOErrorType)
+import System.Posix.Types
+
+#if MIN_VERSION_base(4,7,0)
+import Data.Proxy (Proxy)
+#endif
+
+#if MIN_VERSION_base(4,8,0)
+import Data.Functor.Identity (Identity)
+import Data.Monoid (Alt)
+import Data.Void (Void)
+import Numeric.Natural (Natural)
+#endif
 
 --------------------------------------------------------------------------------
 -- Generic show
 --------------------------------------------------------------------------------
+
+intersperse :: a -> [a] -> [a]
+intersperse _ []    = []
+intersperse _ [h]   = [h]
+intersperse x (h:t) = h : x : (intersperse x t)
 
 appPrec :: Int
 appPrec = 2
@@ -123,28 +160,377 @@ class GShow a where
   gshowsPrec = gshowsPrecdefault
 #endif
 
-instance (GShow a) => GShow (Maybe a) where
-  gshowsPrec = gshowsPrecdefault
-
 gshowsPrecdefault :: (Generic a, GShow' (Rep a))
                   => Int -> a -> ShowS
 gshowsPrecdefault n = gshowsPrec' Pref n . from
 
 
 -- Base types instances
-instance GShow Char   where gshowsPrec = showsPrec
-instance GShow Int    where gshowsPrec = showsPrec
-instance GShow Float  where gshowsPrec = showsPrec
-instance GShow String where gshowsPrec = showsPrec
-instance GShow Bool   where gshowsPrec = showsPrec
+-- Base types instances
+instance GShow () where
+  gshowsPrec = gshowsPrecdefault
 
-intersperse :: a -> [a] -> [a]
-intersperse _ []    = []
-intersperse _ [h]   = [h]
-intersperse x (h:t) = h : x : (intersperse x t)
+instance (GShow a, GShow b) => GShow (a, b) where
+  gshowsPrec = gshowsPrecdefault
 
-instance (GShow a) => GShow [a] where
+instance (GShow a, GShow b, GShow c) => GShow (a, b, c) where
+  gshowsPrec = gshowsPrecdefault
+
+instance (GShow a, GShow b, GShow c, GShow d) => GShow (a, b, c, d) where
+  gshowsPrec = gshowsPrecdefault
+
+instance (GShow a, GShow b, GShow c, GShow d, GShow e) => GShow (a, b, c, d, e) where
+  gshowsPrec = gshowsPrecdefault
+
+instance (GShow a, GShow b, GShow c, GShow d, GShow e, GShow f)
+    => GShow (a, b, c, d, e, f) where
+  gshowsPrec = gshowsPrecdefault
+
+instance (GShow a, GShow b, GShow c, GShow d, GShow e, GShow f, GShow g)
+    => GShow (a, b, c, d, e, f, g) where
+  gshowsPrec = gshowsPrecdefault
+
+instance
+#if __GLASGOW_HASKELL__ >= 709
+    {-# OVERLAPPABLE #-}
+#endif
+    (GShow a) => GShow [a] where
   gshowsPrec _ l =   showChar '['
                    . foldr (.) id
                       (intersperse (showChar ',') (map (gshowsPrec 0) l))
                    . showChar ']'
+
+instance (GShow (f p), GShow (g p)) => GShow ((f :+: g) p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance (GShow (f p), GShow (g p)) => GShow ((f :*: g) p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow (f (g p)) => GShow ((f :.: g) p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow All where
+  gshowsPrec = gshowsPrecdefault
+
+#if MIN_VERSION_base(4,8,0)
+instance GShow (f a) => GShow (Alt f a) where
+  gshowsPrec = gshowsPrecdefault
+#endif
+
+instance GShow Any where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow Arity where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow Associativity where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow Bool where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow BufferMode where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_CC_T)
+instance GShow CCc where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CChar where
+  gshowsPrec = showsPrec
+
+instance GShow CClock where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_DEV_T)
+instance GShow CDev where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CDouble where
+  gshowsPrec = showsPrec
+
+instance GShow CFloat where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_GID_T)
+instance GShow CGid where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow Char where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_INO_T)
+instance GShow CIno where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CInt where
+  gshowsPrec = showsPrec
+
+instance GShow CIntMax where
+  gshowsPrec = showsPrec
+
+instance GShow CIntPtr where
+  gshowsPrec = showsPrec
+
+instance GShow CLLong where
+  gshowsPrec = showsPrec
+
+instance GShow CLong where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_MODE_T)
+instance GShow CMode where
+  gshowsPrec = showsPrec
+#endif
+
+#if defined(HTYPE_NLINK_T)
+instance GShow CNlink where
+  gshowsPrec = showsPrec
+#endif
+
+#if defined(HTYPE_OFF_T)
+instance GShow COff where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow a => GShow (Const a b) where
+  gshowsPrec = gshowsPrecdefault
+
+#if defined(HTYPE_PID_T)
+instance GShow CPid where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CPtrdiff where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_RLIM_T)
+instance GShow CRLim where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CSChar where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_SPEED_T)
+instance GShow CSpeed where
+  gshowsPrec = showsPrec
+#endif
+
+#if MIN_VERSION_base(4,4,0)
+instance GShow CSUSeconds where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CShort where
+  gshowsPrec = showsPrec
+
+instance GShow CSigAtomic where
+  gshowsPrec = showsPrec
+
+instance GShow CSize where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_SSIZE_T)
+instance GShow CSsize where
+  gshowsPrec = showsPrec
+#endif
+
+#if defined(HTYPE_TCFLAG_T)
+instance GShow CTcflag where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CTime where
+  gshowsPrec = showsPrec
+
+instance GShow CUChar where
+  gshowsPrec = showsPrec
+
+#if defined(HTYPE_UID_T)
+instance GShow CUid where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CUInt where
+  gshowsPrec = showsPrec
+
+instance GShow CUIntMax where
+  gshowsPrec = showsPrec
+
+instance GShow CUIntPtr where
+  gshowsPrec = showsPrec
+
+instance GShow CULLong where
+  gshowsPrec = showsPrec
+
+instance GShow CULong where
+  gshowsPrec = showsPrec
+
+#if MIN_VERSION_base(4,4,0)
+instance GShow CUSeconds where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow CUShort where
+  gshowsPrec = showsPrec
+
+instance GShow CWchar where
+  gshowsPrec = showsPrec
+
+instance GShow Double where
+  gshowsPrec = showsPrec
+
+instance GShow a => GShow (Dual a) where
+  gshowsPrec = gshowsPrecdefault
+
+instance (GShow a, GShow b) => GShow (Either a b) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow ExitCode where
+  gshowsPrec = showsPrec
+
+instance GShow Fd where
+  gshowsPrec = showsPrec
+
+instance GShow a => GShow (First a) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow Fixity where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow Float where
+  gshowsPrec = showsPrec
+
+instance GShow (ForeignPtr a) where
+  gshowsPrec = showsPrec
+
+instance GShow (FunPtr a) where
+  gshowsPrec = showsPrec
+
+instance GShow GeneralCategory where
+  gshowsPrec = showsPrec
+
+instance GShow Handle where
+  gshowsPrec = showsPrec
+
+instance GShow HandlePosn where
+  gshowsPrec = showsPrec
+
+#if MIN_VERSION_base(4,8,0)
+instance GShow a => GShow (Identity a) where
+  gshowsPrec = gshowsPrecdefault
+#endif
+
+instance GShow Int where
+  gshowsPrec = showsPrec
+
+instance GShow Int8 where
+  gshowsPrec = showsPrec
+
+instance GShow Int16 where
+  gshowsPrec = showsPrec
+
+instance GShow Int32 where
+  gshowsPrec = showsPrec
+
+instance GShow Int64 where
+  gshowsPrec = showsPrec
+
+instance GShow Integer where
+  gshowsPrec = showsPrec
+
+instance GShow IntPtr where
+  gshowsPrec = showsPrec
+
+instance GShow IOError where
+  gshowsPrec = showsPrec
+
+instance GShow IOErrorType where
+  gshowsPrec = showsPrec
+
+instance GShow IOMode where
+  gshowsPrec = showsPrec
+
+instance GShow c => GShow (K1 i c p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow a => GShow (Last a) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow (f p) => GShow (M1 i c f p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow a => GShow (Maybe a) where
+  gshowsPrec = gshowsPrecdefault
+
+#if MIN_VERSION_base(4,8,0)
+instance GShow Natural where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow Ordering where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow p => GShow (Par1 p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow a => GShow (Product a) where
+  gshowsPrec = gshowsPrecdefault
+
+#if MIN_VERSION_base(4,7,0)
+instance GShow (Proxy s) where
+  gshowsPrec = gshowsPrecdefault
+#endif
+
+instance GShow (Ptr a) where
+  gshowsPrec = showsPrec
+
+instance GShow (f p) => GShow (Rec1 f p) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow SeekMode where
+  gshowsPrec = showsPrec
+
+instance
+#if __GLASGOW_HASKELL__ >= 709
+    {-# OVERLAPPING #-}
+#endif
+    GShow String where
+  gshowsPrec = showsPrec
+
+instance GShow a => GShow (Sum a) where
+  gshowsPrec = gshowsPrecdefault
+
+instance GShow (U1 p) where
+  gshowsPrec = gshowsPrecdefault
+
+#if MIN_VERSION_base(4,8,0)
+instance GShow Void where
+  gshowsPrec = showsPrec
+#endif
+
+instance GShow Word where
+  gshowsPrec = showsPrec
+
+instance GShow Word8 where
+  gshowsPrec = showsPrec
+
+instance GShow Word16 where
+  gshowsPrec = showsPrec
+
+instance GShow Word32 where
+  gshowsPrec = showsPrec
+
+instance GShow Word64 where
+  gshowsPrec = showsPrec
+
+instance GShow WordPtr where
+  gshowsPrec = showsPrec
+
+instance GShow a => GShow (ZipList a) where
+  gshowsPrec = gshowsPrecdefault
