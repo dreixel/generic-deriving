4532630c876dd6e37fe2b16b5cc72998db2b86a0
David Baynard <davidbaynard@gmail.com>
2018-12-18 19:28:22 +0000

    Add data types for tests

--- tests/DefaultSpec.hs
+++ tests/DefaultSpec.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      : DefaultSpec
--- Description : Ensure that deriving via Default newtype works
+-- Description : Ensure that deriving via (Default  a)newtype works
 -- License     : BSD-3-Clause
 --
 -- Maintainer  : generics@haskell.org
@@ -8,11 +8,13 @@
 -- Portability : non-portable
 --
 -- Tests DerivingVia on GHC versions 8.6 and above.
+{-# LANGUAGE DerivingVia #-}
 
 module DefaultSpec (main, spec) where
 
 import Test.Hspec
 import Generics.Deriving.Default
+import Generics.Deriving
 
 main :: IO ()
 main = hspec spec
@@ -21,3 +23,15 @@ spec :: Spec
 spec = do
   describe "DerivingVia Default" $ do
     undefined
+
+newtype TestEq = TestEq Bool
+  deriving (Eq) via (Default Bool)
+newtype TestEnum = TestEnum Bool
+  deriving (Enum) via (Default Bool)
+newtype TestShow = TestShow Bool
+  deriving (Show) via (Default Bool)
+
+newtype TestFoldable a = TestFoldable (Maybe a)
+  deriving (Foldable) via (Default1 Maybe)
+newtype TestFunctor a = TestFunctor (Maybe a)
+  deriving (Functor) via (Default1 Maybe)
