a2902e07a51f954b8dbf93dee299cd13ff9a6da3
David Baynard <davidbaynard@gmail.com>
2019-01-22 15:38:45 +0000

    Improve docs for Default module instances

--- src/Generics/Deriving/Default.hs
+++ src/Generics/Deriving/Default.hs
@@ -91,14 +91,6 @@ newtype Default1 f a = Default1 { unDefault1 :: f a }
 -- Eq
 --------------------------------------------------------------------------------
 
--- |
---
--- @
--- instance (Generic a, GEq' (Rep a)) => Eq (Default a) where
---   (==) :: Default a -> Default a -> Bool
---   Default x == Default y = x `geqdefault` y
--- @
---
 instance (Generic a, GEq' (Rep a)) => GEq (Default a) where
   -- geq :: Default a -> Default a -> Bool
   Default x `geq` Default y = x `geqdefault` y
@@ -107,17 +99,9 @@ instance (Generic a, GEq' (Rep a)) => GEq (Default a) where
 -- Enum
 --------------------------------------------------------------------------------
 
--- |
---
--- @
--- instance (Generic a, GEq a, Enum' (Rep a)) => Enum (Default a) where
---   toEnum :: Int -> Default a
---   toEnum = Default . toEnumDefault
---
---   fromEnum :: Default a -> Int
---   fromEnum (Default x) = fromEnumDefault x
--- @
---
+-- | The 'Enum' class in 'base' is slightly different; it comprises 'toEnum' and
+-- 'fromEnum'. 'Generics.Deriving.Enum' provides functions 'toEnumDefault'
+-- and 'fromEnumDefault'.
 instance (Generic a, GEq a, Enum' (Rep a)) => GEnum (Default a) where
   -- genum :: [Default a]
   genum = Default . to <$> enum'
@@ -126,14 +110,15 @@ instance (Generic a, GEq a, Enum' (Rep a)) => GEnum (Default a) where
 -- Show
 --------------------------------------------------------------------------------
 
--- |
+-- | For example, with this type:
 --
 -- @
--- instance (Generic a, GShow' (Rep a)) => Show (Default a) where
---   showsPrec :: Int -> Default a -> ShowS
---   showsPrec = gshowsPrec
+-- newtype TestShow = TestShow Bool
+--   deriving (GShow) via (Default Bool)
 -- @
 --
+-- `gshow` for `TestShow` would produce the same string as `gshow` for
+-- `Bool`.
 instance (Generic a, GShow' (Rep a)) => GShow (Default a) where
   -- gshowsPrec :: Int -> Default a -> ShowS
   gshowsPrec n (Default x) = gshowsPrecdefault n x
@@ -142,12 +127,17 @@ instance (Generic a, GShow' (Rep a)) => GShow (Default a) where
 -- Semigroup
 --------------------------------------------------------------------------------
 
--- |
+-- | Semigroups often have many sensible implementations of `gsappend`, and
+-- therefore no sensible default. Indeed, there is no 'GSemigroup''
+-- instance for representations of sum types.
+--
+-- In other cases, one may wish to use the existing wrapper newtypes in
+-- 'base', such as the following (using 'Data.Semigroup.First'):
 --
 -- @
--- instance (Generic a, GSemigroup' (Rep a)) => Semigroup (Default a) where
---   (<>) :: Default a -> Default a -> Default a
---   (<>) = gsappend
+-- newtype FirstSemigroup = FirstSemigroup Bool
+--   deriving stock (Eq, Show)
+--   deriving (GSemigroup) via (First Bool)
 -- @
 --
 instance (Generic a, GSemigroup' (Rep a)) => GSemigroup (Default a) where
@@ -158,22 +148,6 @@ instance (Generic a, GSemigroup' (Rep a)) => GSemigroup (Default a) where
 -- Monoid
 --------------------------------------------------------------------------------
 
--- |
---
--- @
--- #if (MIN_VERSION_base(4,11,0))
--- instance (Generic a, GMonoid' (Rep a), Semigroup a, GSemigroup' (Rep a)) => Monoid (Default a) where
--- #else
--- instance (Generic a, GMonoid' (Rep a)) => Monoid (Default a) where
--- #endif
---
---   mempty :: Default a
---   mempty = gmempty
---
---   mappend :: Default a -> Default a -> Default a
---   mappend = gmappend
--- @
---
 instance (Generic a, GMonoid' (Rep a)) => GMonoid (Default a) where
   -- gmempty :: Default a
   gmempty = Default gmemptydefault
@@ -205,13 +179,6 @@ instance (Generic a, Uniplate' (Rep a) a, Context' (Rep a) a) => Uniplate (Defau
 -- Functor
 --------------------------------------------------------------------------------
 
--- |
---
--- @
--- instance (Generic1 f, GFunctor' (Rep1 f)) => Functor (Default1 f) where
---   fmap = gmap
--- @
---
 instance (Generic1 f, GFunctor' (Rep1 f)) => GFunctor (Default1 f) where
   -- gmap :: (a -> b) -> (Default1 f) a -> (Default1 f) b
   gmap f (Default1 fx) = Default1 $ gmapdefault f fx
@@ -228,30 +195,6 @@ instance (Generic1 f, GCopoint' (Rep1 f)) => GCopoint (Default1 f) where
 -- Foldable
 --------------------------------------------------
 
--- |
---
--- @
--- instance (Generic1 t, GFoldable' (Rep1 t)) => Foldable (Default1 t) where
---
---   foldMap :: (Monoid m) => (a -> m) -> Default1 t a -> m
---   fold    ::  Monoid m              => Default1 t m -> m
---   foldr   :: (a -> b -> b)    -> b  -> Default1 t a -> b
---   foldr'  :: (a -> b -> b)    -> b  -> Default1 t a -> b
---   foldl   :: (a -> b -> a)    -> a  -> Default1 t b -> a
---   foldl'  :: (a -> b -> a)    -> a  -> Default1 t b -> a
---   foldr1  :: (a -> a -> a)          -> Default1 t a -> a
---   foldl1  :: (a -> a -> a)          -> Default1 t a -> a
---
---   foldMap = gfoldMap
---   fold    = gfold
---   foldr   = gfoldr
---   foldr'  = gfoldr'
---   foldl   = gfoldl
---   foldl'  = gfoldl'
---   foldr1  = gfoldr1
---   foldl1  = gfoldl1
--- @
---
 instance (Generic1 t, GFoldable' (Rep1 t)) => GFoldable (Default1 t) where
   -- gfoldMap :: Monoid m => (a -> m) -> Default1 t a -> m
   gfoldMap f (Default1 tx) = gfoldMapdefault f tx
@@ -260,22 +203,6 @@ instance (Generic1 t, GFoldable' (Rep1 t)) => GFoldable (Default1 t) where
 -- Traversable
 --------------------------------------------------
 
--- |
---
--- @
--- instance (Generic1 t, GFunctor' (Rep1 t), GFoldable' (Rep1 t), GTraversable' (Rep1 t)) => Traversable (Default1 t) where
---
---   traverse  :: Applicative f => (a -> f b) -> Default1 t    a  -> f (Default1 t b)
---   sequenceA :: Applicative f =>               Default1 t (f a) -> f (Default1 t a)
---   mapM      ::       Monad m => (a -> m b) -> Default1 t    a  -> m (Default1 t b)
---   sequence  ::       Monad m =>               Default1 t (m a) -> m (Default1 t a)
---
---   sequenceA = gsequenceA
---   mapM      = gmapM
---   sequence  = gsequence
---   traverse  = gtraverse
--- @
---
 instance (Generic1 t, GFunctor' (Rep1 t), GFoldable' (Rep1 t), GTraversable' (Rep1 t)) => GTraversable (Default1 t) where
   -- gtraverse :: Applicative f => (a -> f b) -> Default1 t a -> f (Default1 t b)
   gtraverse f (Default1 fx) = Default1 <$> gtraversedefault f fx
